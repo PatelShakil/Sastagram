9efb9384be7ce906f8c5524c1bab55d0abb4c8b0
aamirbhai2003 <aamirbhai2003@gmail.com>
2022-11-01 16:11:36 +0530

    fragment

--- app/src/main/java/com/genie/social_media/fragments/ChatsFragment.kt
+++ app/src/main/java/com/genie/social_media/fragments/ChatsFragment.kt
@@ -8,11 +8,16 @@ import android.view.ViewGroup
 import com.genie.R
 
 class ChatsFragment : Fragment() {
+
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_chats, container, false)
+
     }
 }
